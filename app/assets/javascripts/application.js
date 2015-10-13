67629237fbaa4d964b42ae8847a1bb5b8b1add4c
Ryan Graham <ryangrahamwpb@gmail.com>
2015-10-13 18:30:20 -0400

    Pop modal doent have content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,35 +14,32 @@
 //= require jquery_ujs
 //= require d3
 //= require_tree .
-// function updateCourse(button) {
-//   var =   data: { disable_with: "Course Updating..." }
-// }
-
-
-
-// function hideLastEl() {
-// var length = document.getElementsByClassName("row").length;
-// var lastElement = document.getElementsByClassName("row")[length-1];
-// lastElement.style.display = "none";
-// }
-function upDateCourse() {
-  $('.btn').last().attr("disable",true)
-}
-$( upDateCourse);
-
-function hidelastrow() {
-  $('.association.container').last().hide()
+function updateCourse() {
+  $(".btn").last().attr("disabled", true)
+  $(".edit_course").submit()
 }
 
-$(hidelastrow);
+$(function (){
+  $(".btn").last().on("click", updateCourse)
+});
+
+function hideLastRow() {
+  last = $(".association.container").last()
+  if(last) {
+    last.hide();
+  }
+}
+
+$(hideLastRow);
+
 function showLastRow() {
-  $('#add_grade').click(function(){
-      $('.association.container').last().show()
-  })
+  $(".association.container").last().show();
 }
-$(showLastRow);
 
-// When a Delete button is clicked, hide the row to which it belongs, but mark the hidden _destroy field in that row as truthy.
+$(function (){
+  $(".new-association").on("click", showLastRow);
+});
+
 function hideDeleteRow(){
   $(event.target).closest(".association.container").hide();
   $(event.target).siblings().last().prop("checked", true);
@@ -50,6 +47,7 @@ function hideDeleteRow(){
 function clickAndDelete() {
   $(".btn-danger").on("click", hideDeleteRow);
 }
+
 $(clickAndDelete);
 
 $(function() {
@@ -62,4 +60,16 @@ $(function() {
           scrollTop: target.offset().top
         }, 1000);
         return false;
-Add Comment Click to expand inline 
+      }
+    }
+  });
+});
+
+
+function topModal() {
+  $('#super_modal').modal('show')
+}
+
+$(function(){
+  $(".fa.fa-calendar").on("click", topModal)
+});
